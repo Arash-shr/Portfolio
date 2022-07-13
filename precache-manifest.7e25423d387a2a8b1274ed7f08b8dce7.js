f3085b27fba387feb73bdac1f529500fcb1b6188
arash shirsavar <arash@arashs-mbp.mynet>
2022-07-13 19:44:41 +0100

    Updates

---
+++ precache-manifest.7e25423d387a2a8b1274ed7f08b8dce7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f92e8d25410b3935a51",
+    "url": "/Arash-shr/Portfolio/static/css/main.2ea94ccc.chunk.css"
+  },
+  {
+    "revision": "1f92e8d25410b3935a51",
+    "url": "/Arash-shr/Portfolio/static/js/main.1f92e8d2.chunk.js"
+  },
+  {
+    "revision": "2e8f1a6b9f28a667d85d",
+    "url": "/Arash-shr/Portfolio/static/js/1.2e8f1a6b.chunk.js"
+  },
+  {
+    "revision": "e28b4bed3728b522184e",
+    "url": "/Arash-shr/Portfolio/static/css/2.2ac85aac.chunk.css"
+  },
+  {
+    "revision": "e28b4bed3728b522184e",
+    "url": "/Arash-shr/Portfolio/static/js/2.e28b4bed.chunk.js"
+  },
+  {
+    "revision": "59ab99f7a03612dc57c1",
+    "url": "/Arash-shr/Portfolio/static/js/runtime~main.59ab99f7.js"
+  },
+  {
+    "revision": "274f06e1d794aa89cee96cbccd03e63d",
+    "url": "/Arash-shr/Portfolio/index.html"
+  }
+];
