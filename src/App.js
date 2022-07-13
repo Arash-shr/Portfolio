e42397052a20ff1627331f840c2e7599c6f0414a
arash shirsavar <arash@arashs-mbp.mynet>
2022-07-13 20:19:41 +0100

    Change BroswerRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import "./styles/App.css"
 import Home from "./pages/Home";
 import Experience from "./pages/Experience";
