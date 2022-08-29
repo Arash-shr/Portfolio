f9f8050d75687166e5369b3aed60f2a277754e2a
arash shirsavar <arash@arashs-MacBook-Pro.local>
2022-08-29 18:22:38 +0300

    Updates

---
+++ precache-manifest.c6f4f4d937044f68a44e89e130784f92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "aced94499e0d52d7da7f",
+    "url": "/Portfolio/static/css/main.60c695eb.chunk.css"
+  },
+  {
+    "revision": "aced94499e0d52d7da7f",
+    "url": "/Portfolio/static/js/main.aced9449.chunk.js"
+  },
+  {
+    "revision": "fbd0924dd8ee322e9bc2",
+    "url": "/Portfolio/static/js/1.fbd0924d.chunk.js"
+  },
+  {
+    "revision": "ef35d7ed19e0c8a0d1d9",
+    "url": "/Portfolio/static/css/2.3163ca0f.chunk.css"
+  },
+  {
+    "revision": "ef35d7ed19e0c8a0d1d9",
+    "url": "/Portfolio/static/js/2.ef35d7ed.chunk.js"
+  },
+  {
+    "revision": "511e1b8f6b62f0aae3fc",
+    "url": "/Portfolio/static/js/runtime~main.511e1b8f.js"
+  },
+  {
+    "revision": "a8eb4bad996445e38e56845c501082b5",
+    "url": "/Portfolio/index.html"
+  }
+];
