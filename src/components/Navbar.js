10118b6f05d5852fb748bb3a68b76a9e4d2dd0dd
Arash Shirsavar <arash.shr92@gmail.com>
2022-09-07 19:35:12 +0300

    Change default DarkMode to true

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ import DarkModeToggle from "react-dark-mode-toggle";
 function Navbar() {
   const [navbarExpand, setNavbarExpand] = useState(false);
   const location = useLocation();
-  const [isDarkMode, setIsDarkMode] = useState(() => false);
+  const [isDarkMode, setIsDarkMode] = useState(() => true);
 
   useEffect(() => {
     isDarkMode ? document.documentElement.style.setProperty('--background-color', "linear-gradient(to right, #141e30, #243b55)") : document.documentElement.style.setProperty('--background-color', "linear-gradient(to right,#82a4e0, #95a5bf)");
